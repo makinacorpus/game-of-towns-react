e11e7f6b62ebaa4a33c0e5f3e5cec143d432732b
Sbats <simon.bats12@gmail.com>
2016-10-31 15:29:42 +0100

    Put form in a separate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import AutoComplete from './AutoComplete';
+import CityForm from './CityForm';
 import './App.css';
 
 class App extends Component {
@@ -8,18 +8,13 @@ class App extends Component {
     super();
 
     this.state = {
-      towns: new Set([]),
-      selectedCity: null
+      towns: new Set([])
     };
 
     this.addTown = this.addTown.bind(this);
-    this.selectCity = this.selectCity.bind(this);
   }
 
-  addTown(evt) {
-    evt.preventDefault();
-    const town = this.state.selectedCity;
-
+  addTown(town) {
     if (town) {
       const newSet = new Set(this.state.towns);
       newSet.add(town);
@@ -29,42 +24,15 @@ class App extends Component {
     }
   }
 
-  selectCity(city) {
-    if (city) {
-      this.setState({
-        selectedCity: city
-      });
-    } else {
-      this.setState({
-        selectedCity: null
-      });
-    }
-  }
-
   render() {
     console.log(this.state);
-    const selectedCity = this.state.selectedCity
     return (
       <div className="App">
         <div className="App-header">
           <h2>Game of towns</h2>
         </div>
         <div className="App-content">
-          <form className="city-add" onSubmit={this.addTown}>
-            <label>
-              <p>Ajouter une ville au comparateur</p>
-              <AutoComplete
-                name={'city'}
-                placeholder={'Ville'}
-                selectCity={this.selectCity}/>
-            </label>
-            <button
-              type="submit"
-              disabled={!selectedCity}
-              className={!selectedCity ? 'disabled' : ''}>
-              Ajouter
-            </button>
-          </form>
+          <CityForm onSubmit={this.addTown}/>
         </div>
       </div>
     );
---
+++ src/CityForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import AutoComplete from './AutoComplete';
+import './CityForm.css';
+
+class CityForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectedCity: null
+    };
+
+    this.selectCity = this.selectCity.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  selectCity(city) {
+    if (city) {
+      this.setState({
+        selectedCity: city
+      });
+    } else {
+      this.setState({
+        selectedCity: null
+      });
+    }
+  }
+
+  onSubmit(evt) {
+    evt.preventDefault();
+    const town = this.state.selectedCity;
+
+    if (town) {
+      this.props.onSubmit(town);
+    }
+  }
+
+  render() {
+    const selectedCity = this.state.selectedCity;
+    return(
+      <form className="city-add" onSubmit={this.onSubmit}>
+        <label>
+          <p>Ajouter une ville au comparateur</p>
+          <AutoComplete
+            name={'city'}
+            placeholder={'Ville'}
+            selectCity={this.selectCity}/>
+        </label>
+        <button
+          type="submit"
+          disabled={!selectedCity}
+          className={!selectedCity ? 'disabled' : ''}>
+          Ajouter
+        </button>
+      </form>
+    );
+  }
+}
+
+export default CityForm;

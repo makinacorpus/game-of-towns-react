f53f84edb45fa2e655a5045b461875d6f399c376
Sbats <simon.bats12@gmail.com>
2016-10-31 16:41:22 +0100

    Setup leaflet images use

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import './index.css';
+
+L.Icon.Default.imagePath = '/images/';
 
 ReactDOM.render(
   <App />,

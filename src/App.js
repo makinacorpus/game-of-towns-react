7baec0897d9b5a8414e2a9df8457a0e42aa2fb58
Sbats <simon.bats12@gmail.com>
2016-10-31 12:12:22 +0100

    Create AutoComplete component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import debounce from 'lodash.debounce';
+import AutoComplete from './AutoComplete';
 import './App.css';
 
 class App extends Component {
@@ -8,15 +8,10 @@ class App extends Component {
     super();
 
     this.state = {
-      towns: new Set([]),
-      autoComplete: null,
-      cityInput: ''
+      towns: new Set([])
     };
 
     this.addTown = this.addTown.bind(this);
-    this.autoComplete = this.autoComplete.bind(this);
-    this.getCompletion = debounce(this.getCompletion.bind(this), 500);
-    this.selectCity = this.selectCity.bind(this);
   }
 
   addTown(evt) {
@@ -32,54 +27,8 @@ class App extends Component {
     }
   }
 
-  getCompletion(query) {
-    const url = `http://photon.komoot.de/api/?q=${query}&limit=5`;
-    fetch(url)
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          autoComplete: json.features
-        });
-      })
-      .catch(err => console.error(err));
-  }
-
-  autoComplete(evt) {
-    const query = evt.target.value;
-    this.setState({
-      cityInput: query
-    });
-    if (query.length >= 3) {
-      this.getCompletion(query);
-    }
-  }
-
-  selectCity(cityKey) {
-    const city = this.state.autoComplete[cityKey];
-    this.setState({
-      cityInput: city.properties.name
-    }, () => {this.getCompletion(city.properties.name)});
-  }
-
-  renderAutoComplete(results) {
-    return (
-      <ul className="ac-results-list">
-        {
-          Object.keys(results).map(key =>
-            <li key={key} onClick={() => this.selectCity(key)}>
-              {results[key].properties.name}
-            </li>
-          )
-        }
-      </ul>
-    );
-  }
-
   render() {
     console.log(this.state);
-    const cityInput = this.state.cityInput;
-    const acResult = this.state.autoComplete;
-    const showAc = acResult ? Object.keys(acResult).length > 0 : false;
     return (
       <div className="App">
         <div className="App-header">
@@ -89,18 +38,7 @@ class App extends Component {
           <form onSubmit={this.addTown}>
             <label>
               <p>Ajouter une ville au comparateur</p>
-              <div className="ac-input">
-                <input
-                  className="ac-field"
-                  placeholder={'Ville'}
-                  type="text"
-                  ref={'town'}
-                  value={cityInput}
-                  onChange={this.autoComplete} />
-                <div className="ac-results">
-                  { showAc ? this.renderAutoComplete(acResult) : null }
-                </div>
-              </div>
+              <AutoComplete/>
               <input type="submit" value="Ajouter" />
             </label>
           </form>
---
+++ src/AutoComplete.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import debounce from 'lodash.debounce';
+
+class AutoComplete extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      autoComplete: null,
+      cityInput: ''
+    };
+
+    this.autoComplete = this.autoComplete.bind(this);
+    this.getCompletion = debounce(this.getCompletion.bind(this), 500);
+    this.selectCity = this.selectCity.bind(this);
+  }
+
+  getCompletion(query) {
+    const url = `http://photon.komoot.de/api/?q=${query}&limit=5`;
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          autoComplete: json.features
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  autoComplete(evt) {
+    const query = evt.target.value;
+    this.setState({
+      cityInput: query
+    });
+    if (query.length >= 3) {
+      this.getCompletion(query);
+    }
+  }
+
+  selectCity(cityKey) {
+    const city = this.state.autoComplete[cityKey];
+    this.setState({
+      cityInput: city.properties.name
+    }, () => {this.getCompletion(city.properties.name)});
+  }
+
+  renderAutoComplete(results) {
+    return (
+      <ul className="ac-results-list">
+        {
+          Object.keys(results).map(key =>
+            <li key={key} onClick={() => this.selectCity(key)}>
+              {results[key].properties.name}
+            </li>
+          )
+        }
+      </ul>
+    );
+  }
+
+  render() {
+    const cityInput = this.state.cityInput;
+    const acResult = this.state.autoComplete;
+    const showAc = acResult ? Object.keys(acResult).length > 0 : false;
+    return (
+      <div className="ac-input">
+        <input
+          className="ac-field"
+          placeholder={'Ville'}
+          type="text"
+          ref={'town'}
+          value={cityInput}
+          onChange={this.autoComplete} />
+        <div className="ac-results">
+          { showAc ? this.renderAutoComplete(acResult) : null }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AutoComplete;
